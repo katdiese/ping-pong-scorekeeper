b596d6a84a5705690713e5f1d7a647651a7abbb3
Kathy Dieser <katdiese@gmail.com>
2016-04-01 11:27:21 -0600

    added logic

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = angular.module("pingPongApp", []);
 app.controller("pongController", function($scope) {
   $scope.playerOneScore = 0;
   $scope.playerTwoScore = 0;
+  $scope.oneWins = false;
+  $scope.twoWins = false;
+  $scope.oneLose = false;
+  $scope.twoLose = false;
+  $scope.oneServe = true;
+  $scope.twoServe = false;
   $scope.addOnetoOne = function() {
     $scope.playerOneScore ++;
   }
@@ -12,5 +18,33 @@ app.controller("pongController", function($scope) {
   $scope.reset = function() {
     $scope.playerOneScore = 0;
     $scope.playerTwoScore = 0;
+    $scope.oneWins = false;
+    $scope.twoWins = false;
+    $scope.oneLose = false;
+    $scope.twoLose = false;
+  }
+  $scope.winner = function() {
+    if($scope.playerOneScore >= 11) {
+        $scope.oneWins = true;
+        $scope.twoLose = true;
+      } else if ($scope.playerTwoScore >= 11) {
+      $scope.twoWins = true;
+      $scope.oneLose = true;
+    }
+  }
+  $scope.currentServe = 0;
+  $scope.serveTracker = function() {
+    $scope.currentServe++;
+    if($scope.currentServe == 2) {
+      if($scope.oneServe === true) {
+        $scope.currentServe = 0;
+        $scope.oneServe = false;
+        $scope.twoServe = true;
+      } else {
+        $scope.currentServe = 0;
+        $scope.twoServe = false;
+        $scope.oneServe = true;
+      }
+    }
   }
 })
